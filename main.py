94efef24ea78e159e3d06518d6aa027f0937de0c
Deshmukh Sadat <sadatcoder@gmail.com>
2026-02-09 10:40:16 +0530

    Implement factorial calculation in main.py
    Updated comments and added factorial calculation logic.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder:Deshmukh Sadat Anwar Ahmad
+# Date: 30-JAN-2026
 
 print("--- Factorial Finder ---\n")
 
 
-# Write your code here
+# Write your code here:
+a=int(input("Enter the number: "))
+fact = 1
+for x in range(1,a+1):
+    fact = fact*x
+print("The factorial is:",fact)
+
