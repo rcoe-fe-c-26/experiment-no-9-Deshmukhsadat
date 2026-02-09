1f61b4278be8cc1caee9247a6b10b2f65d301a62
Deshmukh Sadat <sadatcoder@gmail.com>
2026-02-09 10:43:52 +0530

    Enhance factorial program with input validation
    Added input validation for negative numbers and updated output format.

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ print("--- Factorial Finder ---\n")
 
 
 # Write your code here:
-a=int(input("Enter the number: "))
+a = int(input("Enter Number: "))
+if a<0:
+    print(f"Factorial of {a} is Not Defined")
 fact = 1
 for x in range(1,a+1):
-    fact = fact*x
-print("The factorial is:",fact)
+    fact = fact*x 
+print(f"Factorial of {a} is",fact)
+
 
